e51662d15f0e62d2577ff975e96e0703dfbb5922
Benjamin Ho <bhgm93@gmail.com>
2022-04-21 10:07:05 +0800

    updated files and added data.csv

--- predict_customer_churn/churn_library.py
+++ predict_customer_churn/churn_library.py
@@ -5,21 +5,20 @@ Author: Benjamin Ho
 Last update: Apr 2022
 """
 
+from sklearn.metrics import plot_roc_curve, classification_report
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import normalize
 import shap
 import joblib
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns; sns.set()
+import seaborn as sns
+sns.set()
 
-from sklearn.preprocessing import normalize
-from sklearn.model_selection import train_test_split
-
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import GridSearchCV
-
-from sklearn.metrics import plot_roc_curve, classification_report
 
 def import_data(pth):
     """
@@ -33,5 +32,15 @@ def import_data(pth):
     """
     return pd.read_csv("./data/bank_data.csv")
 
+
 def perform_eda(df):
-    
+    # visualize data
+    print(df.head())
+    print("The size of the data is: ", df.shape)
+    print(
+        "There are {} categories and {} data rows.".format(
+            df.shape[1],
+            df.shape[0]))
+
+    # check if there is any empty data
+    print(df.isnull().sum())
